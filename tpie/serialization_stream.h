647b16b6cd112c844aefe4b90e472b26ed4c6d28
Mathias Rav <rav@cs.au.dk>
2013-01-16 14:03:42 +0100

    Move serialization_stream.h implementation into serialization_stream.cpp

--- tpie/serialization_stream.h
+++ tpie/serialization_stream.h
@@ -39,80 +39,30 @@ namespace tpie {
 ///////////////////////////////////////////////////////////////////////////////
 class serialization_header {
 public:
-	static memory_size_type header_size() {
-		memory_size_type sz = sizeof(stream_header_t);
-		memory_size_type align = 4096;
-		return (sz + (align-1))/align * align;
-	}
+	static memory_size_type header_size();
 
-	serialization_header(file_accessor::raw_file_accessor & file)
-		: m_fileAccessor(file)
-	{
-		m_header.magic = stream_header_t::magicConst;
-		m_header.version = stream_header_t::versionConst;
-		m_header.size = 0;
-		m_header.cleanClose = 0;
-	}
+	serialization_header(file_accessor::raw_file_accessor & file);
+	~serialization_header();
 
-	void read() {
-		m_fileAccessor.seek_i(0);
-		m_fileAccessor.read_i(&m_header, sizeof(m_header));
-	}
+	void read();
 
-	void write(bool cleanClose) {
-		m_header.cleanClose = cleanClose;
+	void write(bool cleanClose);
 
-		tpie::array<char> headerArea(header_size());
-		std::fill(headerArea.begin(), headerArea.end(), '\x42');
-		char * headerData = reinterpret_cast<char *>(&m_header);
-		std::copy(headerData, sizeof(m_header) + headerData,
-				  headerArea.begin());
+	void verify();
 
-		m_fileAccessor.seek_i(0);
-		m_fileAccessor.write_i(&headerArea[0], headerArea.size());
-	}
+	stream_size_type get_size();
 
-	void verify() {
-		if (m_header.magic != m_header.magicConst)
-			throw stream_exception("Bad header magic");
-		if (m_header.version < m_header.versionConst)
-			throw stream_exception("Stream version too old");
-		if (m_header.version > m_header.versionConst)
-			throw stream_exception("Stream version too new");
-		if (m_header.cleanClose != 1)
-			throw stream_exception("Stream was not closed properly");
-	}
+	void set_size(stream_size_type size);
 
-	stream_size_type get_size() {
-		return m_header.size;
-	}
-
-	void set_size(stream_size_type size) {
-		m_header.size = size;
-	}
-
-	bool get_clean_close() {
-		return m_header.cleanClose;
-	}
+	bool get_clean_close();
 
 private:
 #pragma pack(push, 1)
-	struct stream_header_t {
-		static const uint64_t magicConst = 0xfa340f49edbada67ll;
-		static const uint64_t versionConst = 1;
-
-		uint64_t magic;
-		uint64_t version;
-		uint64_t size;
-		// bool has funny semantics with regards to equality. we want to reject
-		// invalid bool values (>1), but that is not easy to express with a C++
-		// bool variable.
-		char cleanClose;
-		// TODO: add reverse
-	};
+	struct stream_header_t;
 #pragma pack(pop)
 
-	stream_header_t m_header;
+	std::auto_ptr<stream_header_t> m_headerPtr;
+	stream_header_t & m_header;
 
 	file_accessor::raw_file_accessor & m_fileAccessor;
 };
@@ -130,24 +80,9 @@ private:
 	bool m_open;
 
 protected:
-	serialization_writer_base()
-		: m_blocksWritten(0)
-		, m_size(0)
-		, m_open(false)
-	{
-	}
+	serialization_writer_base();
 
-	void open(std::string path, bool reverse) {
-		close();
-		m_fileAccessor.set_cache_hint(access_sequential);
-		m_fileAccessor.open_wo(path);
-		m_blocksWritten = 0;
-		m_size = 0;
-		m_open = true;
-
-		serialization_header header(m_fileAccessor);
-		header.write(false);
-	}
+	void open(std::string path, bool reverse);
 
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief  Write n bytes from memory area s to next block in stream.
@@ -157,23 +92,9 @@ protected:
 	/// \param s  Memory area with data to write.
 	/// \param n  Number of bytes to write.
 	///////////////////////////////////////////////////////////////////////////
-	void write_block(const char * const s, const memory_size_type n) {
-		assert(n <= block_size());
-		stream_size_type offset = m_blocksWritten * block_size();
-		m_fileAccessor.seek_i(serialization_header::header_size() + offset);
-		m_fileAccessor.write_i(s, n);
-		++m_blocksWritten;
-		m_size = offset + n;
-	}
+	void write_block(const char * const s, const memory_size_type n);
 
-	void close() {
-		if (!m_open) return;
-		serialization_header header(m_fileAccessor);
-		header.set_size(m_size);
-		header.write(true);
-		m_fileAccessor.close_i();
-		m_open = false;
-	}
+	void close();
 };
 
 class serialization_writer : public serialization_writer_base {
@@ -183,10 +104,7 @@ private:
 	tpie::array<char> m_block;
 	memory_size_type m_index;
 
-	void write_block() {
-		p_t::write_block(m_block.get(), m_index);
-		m_index = 0;
-	}
+	void write_block();
 
 	class serializer {
 		serialization_writer & wr;
@@ -216,18 +134,9 @@ private:
 	friend class serializer;
 
 public:
-	void open(std::string path) {
-		p_t::open(path, false);
-		m_block.resize(block_size());
-		m_index = 0;
-	}
+	void open(std::string path);
 
-	void close() {
-		if (m_index > 0) write_block();
-		m_block.resize(0);
-		m_index = 0;
-		p_t::close();
-	}
+	void close();
 
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief  Serialize a serializable item and write it to the stream.
@@ -272,50 +181,17 @@ protected:
 	memory_size_type m_index;
 	memory_size_type m_blockSize;
 
-	serialization_reader_base()
-		: m_open(false)
-		, m_size(0)
-		, m_index(0)
-		, m_blockSize(0)
-	{
-	}
+	serialization_reader_base();
 
-	void open(std::string path, bool reverse) {
-		close();
-		m_fileAccessor.set_cache_hint(reverse ? access_normal : access_sequential);
-		m_fileAccessor.open_ro(path);
-		m_open = true;
-		m_block.resize(block_size());
+	void open(std::string path, bool reverse);
 
-		serialization_header header(m_fileAccessor);
-		header.read();
-		header.verify();
-		m_size = header.get_size();
-		// TODO: use raii to ensure we set m_open to false and don't leak fds
-		// when verification fails.
-	}
-
-	void read_block(const stream_size_type blk) {
-		stream_size_type from = blk * block_size();
-		stream_size_type to = std::min(from + block_size(), m_size);
-		if (to <= from) throw end_of_stream_exception();
-		m_index = 0;
-		m_blockSize = to-from;
-		m_fileAccessor.seek_i(serialization_header::header_size()
-							  + from);
-		m_fileAccessor.read_i(m_block.get(), m_blockSize);
-	}
+	void read_block(const stream_size_type blk);
 
 	// Check if EOF is reached, call read_block(blk) to reset m_index/m_blockSize.
 	virtual void next_block() = 0;
 
 public:
-	void close() {
-		if (!m_open) return;
-		m_fileAccessor.close_i();
-		m_open = false;
-		m_block.resize(0);
-	}
+	void close();
 
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief  Read n bytes from stream into buffer starting at s.
@@ -382,30 +258,15 @@ class serialization_reader : public serialization_reader_base {
 	typedef serialization_reader_base p_t;
 	stream_size_type m_blockNumber;
 
-	void read_from(stream_size_type offset) {
-		read_block(offset);
-	}
+	void read_from(stream_size_type offset);
 
 protected:
-	virtual void next_block() /*override*/ {
-		if (m_blockSize == 0) {
-			m_blockNumber = 0;
-		} else {
-			++m_blockNumber;
-		}
-		read_block(m_blockNumber);
-	}
+	virtual void next_block() /*override*/;
 
 public:
-	serialization_reader()
-		: m_blockNumber(0)
-	{
-	}
+	serialization_reader();
 
-	void open(std::string path) {
-		p_t::open(path, false);
-		m_blockNumber = 0;
-	}
+	void open(std::string path);
 
 	bool can_read() {
 		if (m_index < m_blockSize) return true;
